8bc4faa955d98737f4f0b5cf799f7aa500ae3cbb
Colin Snover <github.com@zetafleet.com>
2014-04-20 08:28:17 +0000

    Some fixes and tests for Command

--- tests/lib/leadfoot/Command.js
+++ tests/lib/leadfoot/Command.js
@@ -25,7 +25,7 @@ define([
 				});
 			},
 
-			'basic functionality': function () {
+			'initialisation': function () {
 				var command = new Command(session, null, function () {
 					return util.createPromise('a');
 				});
@@ -33,8 +33,21 @@ define([
 				return command.then(function (returnValue) {
 					assert.strictEqual(this, command, 'The `this` object in callbacks should be the Command object');
 					assert.deepEqual(command.context, [ 'a' ], 'The context of the Command should be set by the initialiser');
-					assert.isUndefined(returnValue, 'The return value of the initialiser should not be exposed to the first callback');
+					assert.deepEqual(returnValue, 'a', 'The return value of the initialiser should be exposed to the first callback');
 				});
+			},
+
+			'child command': function () {
+				var parent = new Command(session).get(require.toUrl('./data/default.html'));
+				var child = parent.getElementByTagName('p');
+
+				return child.then(function (element) {
+						assert.notStrictEqual(child, parent, 'Getting an element should cause a new Command to be created');
+						assert.isObject(element, 'Element should be provided to first callback of new Command');
+					}).getTagName()
+					.then(function (tagName) {
+						assert.deepEqual(tagName, [ 'p' ], 'Tag name of context element should be provided');
+					});
 			},
 
 			'chain': function () {
@@ -43,7 +56,24 @@ define([
 					.getPageTitle()
 					.then(function (pageTitle) {
 						assert.strictEqual(pageTitle, 'Default & <b>default</b>');
+					})
+					.get(require.toUrl('./data/form.html'))
+					.getPageTitle()
+					.then(function (pageTitle) {
+						assert.strictEqual(pageTitle, 'Form');
 					});
+			},
+
+			'keys': function () {
+				var command = new Command(session);
+				return command.get(require.toUrl('./data/form.html'))
+					.getElementById('input')
+						.clickElement()
+						.type('hello')
+						.getAttribute('value')
+						.then(function (value) {
+							assert.deepEqual(value, [ 'hello' ], 'Typing into a form field should put data in the field');
+						});
 			}
 		};
 	});
